4bd1cc7b2514d2b84c2895470b32eee90021eb14
AngelDaviid <angel_dvalenciae@soy.sena.edu.co>
2025-03-13 19:16:32 -0500

    implementacion de la funcion actualizar

--- routes/clientes.js
+++ routes/clientes.js
@@ -23,6 +23,44 @@ router.get('/', async (req, res) => {
     }
 });
 
+//obtener un solo cliente
+router.get('/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const [ rows ] = await pool.execute(
+            'SELECT * FROM cliente where id_clientes = ?',
+            [id]
+        );
+        if(rows.length > 0){
+            res.json(rows[0]);
+        } else {
+            res.status(404).json({message: 'Cliente no encontrado '});
+        }
+    } catch (error){
+        res.status(500).send(error);
+    }
+});
+
+//actualizar un cliente
+router.put('/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { nombre, email, telefono } = req.body;
+        const [ result ] = await pool.execute(
+            'UPDATE cliente SET nombre = ?, email = ?, telefono = ? WHERE id_clientes = ?',
+            [nombre, email, telefono, id]
+        );
+        if(result.affectedRows > 0){
+            res.json({ id, nombre, email, telefono });
+        } else {
+            res.status(404).json({message: 'Cliente no encontrado '});
+        }
+    } catch (error){
+        res.status(500).send(error);
+    }
+});
+
+
 
 
 module.exports = router;    
